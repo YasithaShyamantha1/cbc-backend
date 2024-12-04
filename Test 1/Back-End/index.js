4dd4502a2e7379923fe11aef9976570865c7e319
IT22608604 <163711404+IT22608604@users.noreply.github.com>
2024-12-04 13:00:46 +0530

    remove unwanted student apis

--- Test 1/Back-End/index.js
+++ Test 1/Back-End/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
-import Student from './Models/Student.js';
 import studentRouter from './routes/studentRouter.js';
 import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
